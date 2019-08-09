cfef94a71592674858973e0c462cbbbdd521f03a
Mike Lofgren <mikeylofgren@gmail.com>
2019-08-09 10:07:00 -0700

    add migration and foreign key.

--- db/migrate/20190809162303_create_products.rb
+++ db/migrate/20190809162303_create_products.rb
@@ -1,6 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      t.column(:name, :string)
+      t.column(:cost, :integer)
+      t.column(:origin, :string)
     end
   end
 end
