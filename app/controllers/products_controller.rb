6f6d18cd50d54a6f5b2056071a4e984db0ee0777
Mike Lofgren <mikeylofgren@gmail.com>
2019-08-09 11:43:29 -0700

    work on controller.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,43 @@
+class ProductsController < ApplicationController
+
+ def index
+   @products = Product.all
+   render :index
+ end
+
+ def new
+  @product = Product.new
+  render :new
+ end
+
+ def create
+  @product = Product.new(product_params)
+  if @product.save
+    redirect_to products_path
+  else
+    render :new
+  end
+ end
+
+ def edit
+
+ end
+
+ def show
+
+ end
+
+ def update
+
+ end
+
+ def destroy
+
+ end
+
+ private
+ def product_params
+   params.require(:product).permit(:name, :cost, :origin)
+ end
+
+end
