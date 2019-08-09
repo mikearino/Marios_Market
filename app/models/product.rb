e0c65c408563940c0a1c15c75de7d28a50e87787
Mike Lofgren <mikeylofgren@gmail.com>
2019-08-09 16:21:21 -0700

    add shoulda matchers.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
+  validates :cost, presence: true
+  validates :origin, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates :rating, presence: true
+  validates :rating, numericality: { less_than_or_equal_to: 5,  only_integer: true }
+  validates_length_of :content_body, minimum: 50, maximum: 250
 end
