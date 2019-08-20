4ff57cd67264ee7fe04c87486640c1b354eb0ca8
Student <example@example.com>
2019-08-20 08:40:52 -0700

    integration passes.

--- spec/features/add_reviews_pages_spec.rb
+++ spec/features/add_reviews_pages_spec.rb
@@ -6,11 +6,13 @@ describe "the add a product process" do
     click_link 'Create new product'
     fill_in 'Name', :with => 'Flan'
     fill_in 'Cost', :with => 10
-    fill_in 'Origin', :with => 'USA'
+    fill_in 'Origin', :with => 'Mexico'
     click_on 'Create Product'
     expect(page).to have_content 'Product successfully added!'
     expect(page).to have_content 'Flan'
+    within('#all')do
     click_on 'Flan'
+  end
     click_on 'Add a review'
     fill_in 'Author', :with => 'Dan'
     fill_in 'Content body', :with => 'fjkdjfdksjffkljdfkjfkjfkdsjfdkfjdkfjdskfjdkfjdkfjdksfjdslakfjsdklfjdskfjkafjkdsjfkljafkdsjfkdsjfkdsjfkdsfjkdfjdfklfjdsklfjdskalfjdskjfkdslfjdskl'
@@ -20,9 +22,20 @@ describe "the add a product process" do
     expect(page).to have_content 'Dan'
   end
 
-  # it "gives an error when no name is entered" do
-  #   visit new_product_path
-  #   click_on 'Create Product'
-  #   expect(page).to have_content "Name can't be blank"
-  # end
+  it "gives an error when no name is entered" do
+    visit products_path
+    click_link 'Create new product'
+    fill_in 'Name', :with => 'Flan'
+    fill_in 'Cost', :with => 10
+    fill_in 'Origin', :with => 'Mexico'
+    click_on 'Create Product'
+    expect(page).to have_content 'Product successfully added!'
+    expect(page).to have_content 'Flan'
+    within('#all')do
+    click_on 'Flan'
+  end
+    click_on 'Add a review'
+    click_on 'Create Review'
+    expect(page).to have_content "Author can't be blank"
+  end
 end
